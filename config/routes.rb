eebb45501d2f3d457ac3875e535a7829069c9c1f
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-24 11:21:42 +0100

    Defined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :assessments do
+    namespace :skills do
+      resources :questions, only: [ :show ]
+    end
+    resources :kasis, only: [ :index, :show ]
+  end
+  resources :professions, only: [ :index, :show ]
+  resources :roadmaps, only: [ :show ]
 end
