8d330772eda315719f8aff58970ddfa3f6fbe725
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-24 11:40:06 +0100

    Fixed error in kasi model (has_many - through)

--- app/models/kasi.rb
+++ app/models/kasi.rb
@@ -1,5 +1,6 @@
 class Kasi < ApplicationRecord
-  has_many :users
+  has_many :user_kasis
+  has_many :users, through: :user_kasis
   has_many :answers, depenedent: :destroy
   validates :category, :name, presence: true
 end
