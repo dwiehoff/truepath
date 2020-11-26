5d75811d219870533edfaccb63101792f6cd9986
Alexander von Croy <alexander@voncroy.eu>
2020-11-26 15:36:09 +0100

    basic functionality/view routed via profession/id

---
+++ app/controllers/professions_controller.rb
@@ -0,0 +1,12 @@
+class ProfessionsController < ApplicationController
+  def show
+    @profession = Profession.find(params[:id])
+  end
+
+private
+def set_profession
+  @profession = Profession.find(params[:id])
+end
+end 
+
+
