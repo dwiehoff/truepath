010e555ffef00e61b95d7b8f5fc428e266ff17c7
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-12-02 19:43:28 +0100

    Dynamically display steps completed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,6 +31,7 @@ require("jquery-ui");
 // import { initSelect2 } from '../components/init_select2';
 import { test } from '../packs/professions'
 import { algorithm } from '../packs/questions'
+import { fill_circles } from '../packs/roadmap'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
@@ -39,4 +40,6 @@ document.addEventListener('turbolinks:load', () => {
     test();
   if (document.querySelector('.questions.index'))
     algorithm();
+  if (document.querySelector('.professions.show'))
+    fill_circles();
 });
