247c30ac860713f64fedbf4be7328a3bff6e93fd
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-12-03 12:14:02 +0100

    Meta tags + 400/500 pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  module ApplicationHelper
+    # def title(text)
+    #   content_for :title, text
+    # end
+
+    # def meta_tag(tag, text)
+    #   content_for :"meta_#{tag}", text
+    # end
+
+    # def yield_meta_tag(tag, default_text = '')
+    #   content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+    # end
+  end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
